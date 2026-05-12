fa10d642bac02942af17d37e282cd71734f833ff
Alex Stute <stute5000@gmail.com>
2026-05-12 02:56:49 -0400

    Got spell data to save alongside character data

--- JS/spells.js
+++ JS/spells.js
@@ -3,30 +3,32 @@ function sleep(ms) {
 }
 
 function initSheet() {
-    // TS.debug.log(document.getElementById('weapons-text').textContent)
     let inputs = document.querySelectorAll("input,button,textarea,select");
     for (let input of inputs) {
-        // TS.debug.log(input.id)
         if (input.id != undefined && input.id != "clear-storage") {
             input.addEventListener("change", function() {
                 onInputChange(input)
-            });
-
-            let titleSibling = findFirstSiblingWithClass(input, "field-title");
-            if (titleSibling != null) {
+            }); 
+            try {
+                let titleSibling = findFirstSiblingWithClass(input, "field-title");
+                if (titleSibling != null) {
                 titleSibling.id = `${input.id}-field-title`;
             }
-            let descSibling = findFirstSiblingWithClass(input, "field-desc");
-            if (descSibling != null) {
-                descSibling.id = `${input.id}-field-desc`;
             }
-
+            catch {titleSibling = null}
+            try{
+                let descSibling = findFirstSiblingWithClass(input, "field-desc");
+                if (descSibling != null) {
+                    descSibling.id = `${input.id}-field-desc`;
+                }
+            }
+            catch {descSibling = null}
             let finalInput = input; //otherwise the input can change which breaks the onchange handler
-            if (titleSibling == null && input.dataset.modifier != undefined) {
-                //manual fix for melee/ranged attack buttons being formatted differently
-                titleSibling = finalInput;
-                finalInput = document.getElementById(finalInput.dataset.modifier);
-            }
+            // if (titleSibling == null && input.dataset.modifier != undefined) {
+            //     //manual fix for melee/ranged attack buttons being formatted differently
+            //     titleSibling = finalInput;
+            //     finalInput = document.getElementById(finalInput.dataset.modifier);
+            // }
 
             if (titleSibling != null && titleSibling.dataset.diceType != undefined) {
                 titleSibling.classList.add("interactible-title");
@@ -52,11 +54,11 @@ function initSheet() {
 function onInputChange(input) {
     TS.debug.log("Input Change Start: " + input.id)
     // console.log(input.id)
-    if (input.id == 'weapons-text') {
-        TS.debug.log("Input type: "+input.type)
-        TS.debug.log("value: "+document.getElementById('weapons-text').value)
-    // TS.debug.log(input.id)
-    }
+    // if (input.id == 'weapons-text') {
+    //     TS.debug.log("Input type: "+input.type)
+    //     TS.debug.log("value: "+document.getElementById('weapons-text').value)
+    // // TS.debug.log(input.id)
+    // }
     //handles input changes to store them in local storage
     calculateRolls();
     let data;
@@ -68,22 +70,26 @@ function onInputChange(input) {
         // TS.debug.log(storedData)
         //parse stored blob as json, but also handle if it's empty by
         //defaulting to an empty json document "{}" if stored data is false
-        data = JSON.parse(storedData || "{}");
+        data = JSON.parse(storedData || "{character={}, spells={}}");
+        // TS.debug.log(data)
+        // TS.debug.log(data['spells'])
+        // TS.debug.log(input.id)
         if (input.type == "checkbox") {
-            data[input.id] = input.checked ? "on" : "off";
+            data['spells'][input.id] = input.checked ? "on" : "off";
         } 
         // else if (input.type == "textarea") {
         //     data[input.id] = input.textContent;
         // } 
         else {
-            data[input.id] = input.value;
+            data['spells'][input.id] = input.value;
+            // TS.debug.log(data['spells'][input.id])
         }
         //set new data, handle response
         TS.localStorage.campaign.setBlob(JSON.stringify(data)).then(() => {
             //if storing the data succeeded, enable the clear storage button
-            clearStorageButton.classList.add("danger");
-            clearStorageButton.disabled = false;
-            clearStorageButton.textContent = "Clear Character Sheet";
+            // clearStorageButton.classList.add("danger");
+            // clearStorageButton.disabled = false;
+            // clearStorageButton.textContent = "Clear Character Sheet";
         }).catch((setBlobResponse) => {
             TS.debug.log("Failed to store change to local storage: " + setBlobResponse.cause);
             console.error("Failed to store change to local storage:", setBlobResponse);
@@ -128,4 +134,54 @@ function onInputChange(input) {
     //     //     createWeapon(weapons[weapon])
     //     // }
     // }
+}
+
+function loadStoredData() {
+    TS.localStorage.campaign.getBlob().then((storedData) => {
+        //localstorage blobs are just unstructured text.
+        //this means we can store whatever we like, but we also need to parse it to use it.
+        let data = JSON.parse(storedData || "{character={}, spells={}}");
+        if (Object.entries(data['spells']).length > 0) {
+            // clearStorageButton.classList.add("danger");
+            // clearStorageButton.disabled = false;
+            // clearStorageButton.textContent = "Clear Character Sheet";
+        }
+        let keyCount = 0;
+        for (let [key, value] of Object.entries(data['spells'])) {
+            keyCount++;
+            let element = document.getElementById(key);
+            element.value = value;
+            if (key == "thac0") {
+                element.dispatchEvent(new Event('change'));
+            } else if (element.type == "checkbox") {
+                element.checked = value == "on" ? true : false;
+            } else if (key == "abilities-text") {
+                let results = parseActions(element.value);
+                addActions(results);
+            } else if (key == "skills-text") {
+                let results = parseSkillsLores(element.value);
+                addSkillLore(results);
+            } else if (key == "Hitdice-text") {
+                let results = parseActions(element.value);
+                addActionsNew(results);
+            }
+        }
+        //adding some log information to the symbiote log
+        //this doesn't have particular importance, but is here to show how it's done
+        TS.debug.log(`Loaded ${keyCount} values from storage`);
+    });
+}
+
+async function onStateChangeEvent(msg) {
+    if (msg.kind === "hasInitialized") {
+        //the TS Symbiote API has initialized and we can begin the setup. think of this as "init".
+        // clearStorageButton = document.getElementById("clear-storage");
+        loadStoredData();
+        await sleep(1000);
+        initSheet();
+    }
+}
+
+function calculateRolls(){
+    return;
 }
